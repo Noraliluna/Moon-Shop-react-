af01237608354754ab6b51607f5e513a87d89cc7
Norali Luna <luna.norali422@gmail.com>
2022-09-12 23:38:09 -0300

    Boton Agregar al Carrito

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -4,17 +4,22 @@ const ItemCount = () => {
     const [rate, setRate] = useState(0);
     
     const handleRate = () => {
-        setRate (rate+1);
+        setRate (rate+1 );
     }
     
     const handleDelete = () => {
         setRate (rate-1);
     }
 
+    const Carro = () =>{
+        alert(`You have select ${rate}`)
+    }
+
     return(
         <div className="count">
             <button  className="btn btn-dark" onClick={handleRate}>+</button>{rate}
             <button  className="btn btn-dark" onClick={handleDelete}>-</button>
+            <button  className="AddWidget" onClick={Carro}>Agregar al carrito</button>
         </div>
     )
 }
